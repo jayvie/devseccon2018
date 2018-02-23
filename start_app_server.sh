aebc95eebe308467ec364403f250b38f6f099712
jayvie <jayvie@jv-kpln-mac.local>
2018-02-23 14:05:20 +0800

    removed secret

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://jayvie:8CUZAUVrb*xSfvTOS7@ds247178.mlab.com:47178/devsecsg2018
+#export MONGODB_CREDENTIALS=mongodb://XXXXXXXXXX:XXXXXXXXXXXXXXX.mlab.com:XXXXXXXX/XXXXXXXX
 
 # finally, run the app in dev mode...
 npm run dev
