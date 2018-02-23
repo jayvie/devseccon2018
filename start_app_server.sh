65a65f194c85026f7d3715f15cebba2354b7968a
jayvie <jayvie@jv-kpln-mac.local>
2018-02-23 13:33:10 +0800

    added mongo credentials

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://test:testtest@ds239638.mlab.com:39638/devseccon2018
+export MONGODB_CREDENTIALS=mongodb://jayvie:8CUZAUVrb*xSfvTOS7@ds247178.mlab.com:47178/devsecsg2018
 
 # finally, run the app in dev mode...
 npm run dev
